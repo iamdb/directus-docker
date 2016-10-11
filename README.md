eb6cafc99d3adc8cb0c9c289ae0638c3e1035a3f
David Benjamin <d@vidbenjamin.com>
2016-10-10 17:42:50 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Dockerized Directus with S3 support
+
+* Create a new database or bring your own
+* Easy S3 support. Just add environment variables to `docker-compose.yml`.
+* Utilizes [custom nginx config](https://github.com/h5bp/server-configs-nginx).
+* php-fpm 5.6, mariadb 10.1 and nginx 1.10
+
+The goal of this is to easily create a high-performing headless content API, with a GUI and CDN support. One that can be launched and configured easily so that you can focus on more important things. This is still a work in progress.
+
+It assumes you're familiar with [docker-machine](https://docs.docker.com/machine/overview/) and [docker-compose](https://docs.docker.com/compose/overview/).
+
+The best way to use this is to mount a local storage volume or add S3 credentials to send files there.
+
+The `nginx` and `fpm` containers are provided separately for flexibility, but there's a sample `docker-compose.yml` file that's provided which should have everything you need to get up and running quickly. They are also built automatically on Docker Hub using this repo at `iamdb/nginx-directus-docker` and `iamdb/fpm-directus-docker`.
+
+## docker-compose.yml
+To get this up and running is really easy.
+
+Just run:
+```
+docker-compose up
+```
+
+It will build and start all of the containers. It may take a while the first time. The `fpm` container has a lot to build. If they all build and start correctly, the `fpm` container will wait for the database host and port to become available. Once it is, and if Directus isn't already installed and all of the admin variables and the site title variable are present, a new installation using that information will be created.
+
+## Goals
+
+* Let's Encrypt support
---
+++ fpm-directus/init.sh
@@ -0,0 +1,42 @@
+#
+#	INIT SCRIPT INSPIRED/SNAGGED FROM https://github.com/beevelop/docker-directus/blob/master/init.sh
+#
+#!/bin/bash
+set -ea
+
+DB_PORT=${DB_PORT:-3306}
+
+while ! nc -z ${DB_HOST} ${DB_PORT}; do
+  echo "Waiting for Database ${DB_HOST}:${DB_PORT}..."
+  sleep 2
+done
+
+if [ ! -f /var/www/html/api/config.php ] && [ -n "${DB_HOST}" ]; then
+    echo "Setting up database and configuration..."
+
+    su-exec www-data php bin/directus install:config -h "${DB_HOST}" -n "${DB_NAME}" -u "${DB_USER}" -p "${DB_PASS}"
+    su-exec www-data php bin/directus install:database
+    su-exec www-data php bin/directus install:install -e "${ADMIN_EMAIL}" -p "${ADMIN_PASSWORD}" -t "${SITE_NAME}"
+fi
+
+if [ -f /var/www/html/api/config.php ] && [ -z $(nc -z ${MEMCACHE_URL} 11211) ]; then
+  echo "memcache server detected... enabling"
+  su-exec www-data sed -i "s|'MEMCACHED_SERVER', '127.0.0.1'|'MEMCACHED_SERVER', '${MEMCACHE_URL}'|g" /var/www/html/api/config.php
+fi
+
+if [ -f /var/www/html/api/config.php ] && [ -n "${DIRECTUS_ENV}" ] && [ ${DIRECTUS_ENV} != "development" ]; then
+  su-exec www-data sed -i "s|'DIRECTUS_ENV', 'development'|'DIRECTUS_ENV', '${DIRECTUS_ENV}'|g" /var/www/html/api/config.php
+  su-exec www-data sed -i "s|'MEMCACHED_ENV_NAMESPACE', 'staging'|'MEMCACHED_ENV_NAMESPACE', '${DIRECTUS_ENV}'|g" /var/www/html/api/config.php
+fi
+
+if [ -f /var/www/html/api/configuration.php ] && [ -n "${S3_KEY}" ] && [ -n "${S3_SECRET}" ] && [ -n "${S3_REGION}" ] && [ -n "${S3_BUCKET}" ]; then
+    su-exec www-data sed -i "s|'adapter' => 'local'|'adapter' => 's3'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|'root' => BASE_PATH . '/storage/uploads'|'root' => '${S3_ROOT}'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|'root_url' => '/storage/uploads'|'root_url' => 'https://s3.amazonaws.com/${S3_BUCKET}${S3_ROOT}'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|'root_thumb_url' => '/storage/uploads/thumbs'|'root_thumb_url' => 'https://s3.amazonaws.com/${S3_BUCKET}${S3_ROOT}/thumbs'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|//   'key'    => 's3-key'|'key'    => '${S3_KEY}'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|//   'secret' => 's3-key'|'secret'    => '${S3_SECRET}'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|//   'region' => 's3-region'|'region'    => '${S3_REGION}'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|//   'version' => 's3-version'|'version'    => 'latest'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|//   'bucket' => 's3-bucket'|'bucket'    => '${S3_BUCKET}'|g" /var/www/html/api/configuration.php
+fi
