995082f691d7d846e265880233688c01e4143315
David Benjamin <d@vidbenjamin.com>
2016-10-18 15:07:57 -0700

    switch to caddy, changes to memcache and s3 support

--- fpm-directus/init.sh
+++ fpm-directus/init.sh
@@ -26,14 +26,15 @@ fi
 
 if [ -f /var/www/html/api/config.php ] && [ -n "${DIRECTUS_ENV}" ] && [ ${DIRECTUS_ENV} != "development" ]; then
   su-exec www-data sed -i "s|'DIRECTUS_ENV', 'development'|'DIRECTUS_ENV', '${DIRECTUS_ENV}'|g" /var/www/html/api/config.php
-  su-exec www-data sed -i "s|'MEMCACHED_ENV_NAMESPACE', 'staging'|'MEMCACHED_ENV_NAMESPACE', '${DIRECTUS_ENV}'|g" /var/www/html/api/config.php
+  MEMCACHE_ENV=$([ "$DIRECTUS_ENV" == "production" ] && echo "prod" || echo "$DIRECTUS_ENV")
+  su-exec www-data sed -i "s|'MEMCACHED_ENV_NAMESPACE', 'staging'|'MEMCACHED_ENV_NAMESPACE', '${MEMCACHE_ENV}'|g" /var/www/html/api/config.php
 fi
 
 if [ -f /var/www/html/api/configuration.php ] && [ -n "${S3_KEY}" ] && [ -n "${S3_SECRET}" ] && [ -n "${S3_REGION}" ] && [ -n "${S3_BUCKET}" ]; then
     su-exec www-data sed -i "s|'adapter' => 'local'|'adapter' => 's3'|g" /var/www/html/api/configuration.php
-    su-exec www-data sed -i "s|'root' => BASE_PATH . '/storage/uploads'|'root' => '${S3_ROOT}'|g" /var/www/html/api/configuration.php
-    su-exec www-data sed -i "s|'root_url' => '/storage/uploads'|'root_url' => 'https://s3.amazonaws.com/${S3_BUCKET}${S3_ROOT}'|g" /var/www/html/api/configuration.php
-    su-exec www-data sed -i "s|'root_thumb_url' => '/storage/uploads/thumbs'|'root_thumb_url' => 'https://s3.amazonaws.com/${S3_BUCKET}${S3_ROOT}/thumbs'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|'root' => BASE_PATH . '/storage/uploads'|'root' => '${S3_ROOT:-"/"}'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|'root_url' => '/storage/uploads'|'root_url' => '//s3.amazonaws.com/${S3_BUCKET}${S3_ROOT}media'|g" /var/www/html/api/configuration.php
+    su-exec www-data sed -i "s|'root_thumb_url' => '/storage/uploads/thumbs'|'root_thumb_url' => '//s3.amazonaws.com/${S3_BUCKET}${S3_ROOT}thumbs'|g" /var/www/html/api/configuration.php
     su-exec www-data sed -i "s|//   'key'    => 's3-key'|'key'    => '${S3_KEY}'|g" /var/www/html/api/configuration.php
     su-exec www-data sed -i "s|//   'secret' => 's3-key'|'secret'    => '${S3_SECRET}'|g" /var/www/html/api/configuration.php
     su-exec www-data sed -i "s|//   'region' => 's3-region'|'region'    => '${S3_REGION}'|g" /var/www/html/api/configuration.php
